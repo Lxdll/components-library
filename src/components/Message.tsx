7dd171b125b9cddf8fac695d67180a83483889eb
Lxdll <209888010@qq.com>
2024-03-26 20:03:43 +0800

    chore: init

---
+++ src/components/Message.tsx
@@ -0,0 +1,16 @@
+/**
+ * @author: yellowlu
+ * description
+ */
+
+import React from 'react'
+
+interface MessageProps {
+
+}
+
+export default function Message (props: MessageProps) {
+  return (
+    <span>shi</span>
+  )
+}
